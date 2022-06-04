cd8c63fcca718a62c38a07b0bb6198d9c25466b7
Adrian Walker <adrianwalker@MacBook-Pro.local>
2022-06-04 16:40:55 -0400

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import ShowList from "./components/ShowList";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+
 
 function App() {
   return (
     <div className="App">
-      <ShowList/>
+      <ShowList />
       Fuck
     </div>
   );
