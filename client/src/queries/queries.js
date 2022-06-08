432b6de131dd3282ad6f409d38bb4dffb1547bfd
Adrian-Walker <awalker3365@gmail.com>
2022-06-08 19:32:27 -0400

    added mutation addShow Query

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -24,4 +24,15 @@ const GET_DIRECTOR_QUERY = gql`
   }
 `;
 
-export { GET_SHOW_QUERY, GET_DIRECTOR_QUERY };
+const ADD_MOVIE_QUERY = gql`
+  {
+    mutation {
+      addShow(name: "", genre: "", directorId: "") {
+        name
+        id
+      }
+    }
+  }
+`;
+
+export { GET_SHOW_QUERY, GET_DIRECTOR_QUERY, ADD_MOVIE_QUERY };
